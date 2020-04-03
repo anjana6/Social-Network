d2a735c0080f078cc9d9a8ffc770685f3e697d3a
anjana6 <anjanashakthi95@gmail.com>
2020-04-03 21:59:58 +0530

    create like and unlike router

--- Network server/models/Post.js
+++ Network server/models/Post.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const postSchema = new mongoose.Schema({
+const postSchema = new Schema({
     user:{
-        type: mongoose.Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "User"
     },
     text:{
@@ -15,6 +16,14 @@ const postSchema = new mongoose.Schema({
     avatar:{
         type:String
     },
+    likes:[
+       {
+           userId:{
+               type: Schema.Types.ObjectId,
+               ref: "User"
+           }
+       }
+    ],
     date:{
         type:Date,
         default: Date.now
--- Network server/routes/postRouter.js
+++ Network server/routes/postRouter.js
@@ -58,11 +58,49 @@ router.get('/:id',auth,async (req,res) =>{
 
 router.delete("/:id",auth,async(req,res) =>{
     try {
-        await Post.delete(req.param.id);
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({msg:"Post Removed"})
     } catch (err){
-        console.log(err.message)
+        console.log(err.message);
+        if(err.kind === "ObjectId"){
+            return res.status(401).json({error:"Post not fond"})
+        }
         res.status(501).send('Server Error');
     }
+});
+
+router.put("/like/:id",auth,async(req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.likes.filter(like => like.userId.toString() === req.userId).length >0){
+            return res.status(400).json({error:"Post alredy liked"})
+        }
+        post.likes.unshift({user:req.userId});
+
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.put("/unlike/:id",auth,async(req,res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(post.likes.filter(like => like.userId.toString() === req.userId).length === 0){
+            return res.status(400).json({msg:"Post has not yet liked"});
+        }
+        
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+        
+    }
 })
+
 
 module.exports = router;
