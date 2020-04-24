4fffc700a699f80aee6d95c64b0422f6c38553fd
anjana6 <anjanashakthi95@gmail.com>
2020-04-24 22:33:30 +0530

    design the about page

--- social_network/src/components/profile/About.vue
+++ social_network/src/components/profile/About.vue
@@ -6,11 +6,103 @@
                     profile Image
                 </v-flex>
                 <v-flex xs8>
-                    <h1 class="display-3">{{profile.user.name}}</h1>
+                    <h1 class="display-3 ma-5">{{profile.user.name}}</h1>
                 </v-flex>
             </v-layout>
         </v-content>
-       
+       <v-content>
+         <v-layout row>
+            <v-flex md4>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-briefcase</v-icon>
+                    </v-avatar>
+                        MObios<br><span>work palce</span>
+                </v-banner>
+            </v-flex>
+            <v-flex md4>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-home</v-icon>
+                    </v-avatar>
+                        MObios<br><span>current city</span>
+                </v-banner>
+            </v-flex>
+            <v-flex md4>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>Home twon</span>
+                </v-banner>
+            </v-flex>
+         </v-layout>
+       </v-content>
+       <v-content>
+        <h2>Education</h2>
+         <v-layout row>
+            <v-flex md6>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>school</span>
+                </v-banner>
+            </v-flex>
+            <v-flex md6>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>campus</span>
+                </v-banner>
+            </v-flex>
+         </v-layout>
+       </v-content>
+        <v-content>
+        <h2>Basic Info</h2>
+         <v-layout row>
+            <v-flex md6>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>birthday</span>
+                </v-banner>
+            </v-flex>
+            <v-flex md6>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>Relijans</span>
+                </v-banner>
+            </v-flex>
+            <v-flex md6>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>gender</span>
+                </v-banner>
+            </v-flex>
+            <v-flex md6>
+                <v-banner two-line>
+                    <v-avatar slot="icon" color="primary accent-4" size="40">
+                    <v-icon color="white">fas fa-map-marker-alt</v-icon>
+                    </v-avatar>
+                        MObios<br><span>sivil status</span>
+                </v-banner>
+            </v-flex>
+         </v-layout>
+       </v-content>
+       <v-content>
+           <v-layout row v-if="!!profile.skills">
+              <v-flex v-for="skill in profile.skills" :key="skill.id" md-3>
+                  {{skill}}
+              </v-flex>
+           </v-layout>
+       </v-content>
 
     </v-container>
 </template>
